75606665672a302eae1017fd37cce4f01d6ee443
tjaques <tjaques@flexngate.com>
2021-06-08 10:11:08 -0400

    added fonts

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,6 @@
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+
 <template>
   <div id="app">
     <div class="flex-wrapper">
