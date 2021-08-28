44c037970c83cea5edee903d9e09d07586dd9e48
tjaques <tjaques@flexngate.com>
2021-08-28 00:48:11 -0400

    resized photos

--- src/components/productList.vue
+++ src/components/productList.vue
@@ -3,7 +3,7 @@
     <div v-for="category in products.categories">
       <h3 class="text-4xl my-4 baskerville">{{ category.category }}</h3>
       <div v-for="sub in category.subcategories">
-        <div class="grid grid-cols-2 md:grid-cols-3">
+        <div class="grid grid-cols-2 lg:grid-cols-4">
           <div class="w-5/6 mb-4" v-for="product in sub.products">
             <img v-if="product.photo" :src="product.photo" :alt="product.name" />
             <p v-else>{{ product.name }}</p>
