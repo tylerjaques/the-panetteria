3b1463cd60b6278b938afb43c31265333109588c
tjaques <tjaques@flexngate.com>
2021-08-30 23:24:48 -0400

    test

--- src/components/alttop.vue
+++ src/components/alttop.vue
@@ -2,7 +2,7 @@
   <div class="top row text-lg md:text-2xl bg-black bg-opacity-50 text-white tracking-wide">
     <div class="cell flex flex-wrap mt-4 md:mt-8 xl:mb-0">
       <div class="mx-4 md:mx-16 mb-4 md:mb-8 w-full">
-          <router-link class="title" to="/"><img src="../../images/Logo.jpg" alt="The Panetteria"></router-link>
+          <router-link class="title" to="/"><img src="/../../images/Logo.jpg" alt="The Panetteria"></router-link>
       </div>
       <div class="flex flex-wrap px-4 md:px-16 pb-2 bg-black bg-opacity-75 w-full">
         <router-link class="page" to="/products">PRODUCTS</router-link>
