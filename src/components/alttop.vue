49465969e2a32858d6a1e5a50b8cc410b73c3d8f
tjaques <tjaques@flexngate.com>
2021-11-06 10:54:15 -0400

    cropped image

--- src/components/alttop.vue
+++ src/components/alttop.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="top row text-lg md:text-2xl bg-black bg-opacity-50 text-white tracking-wide">
-      <div class="flex flex-wrap px-4 md:px-16 pb-2 bg-black w-full">
+      <div class="flex flex-wrap px-4 md:px-16 pb-2 bg-black bg-opacity-75 w-full">
         <router-link class="page" to="/products">PRODUCTS</router-link>
         <router-link class="page" to="/about">ABOUT US</router-link>
         <router-link class="page" to="/contact">CONTACT US</router-link>
