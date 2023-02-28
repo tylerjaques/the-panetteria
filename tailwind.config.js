644675baf2c65e7601816f244da723a833de03a2
Tyler Jaques <Tyler.Jaques@edisonfinancial.ca>
2023-02-28 00:01:03 -0500

    updated dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
