d2a34b520b713b45a1147b850c66b9b101c6a34b
Tyler Jaques <Tyler.Jaques@edisonfinancial.ca>
2023-02-28 08:53:10 -0500

    reverted index

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
-    <div class="bg">
-      <div class="absolute table">
-        <alttop/>
-        <div class="middle text-white">
-          <div class="container cell"></div>
-        </div>
-        <bottom/>
+    <div class="bg" />
+    <div class="absolute table">
+      <alttop />
+      <div class="middle text-white">
+        <div class="container cell"></div>
       </div>
+      <bottom />
     </div>
   </div>
 </template>
@@ -18,34 +17,34 @@ import bottom from '../components/bottom.vue';
 
 export default {
   name: 'Home',
-  components: {alttop, bottom},
+  components: { alttop, bottom },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-@media (max-width: 800px) {
-  .bg {
-    background-image: url('../../public/images/front1.jpg');
-    background-position: center center;
-    background-repeat: no-repeat;
-    position: fixed;
-    height: 100vh;
-    width: 100vw;
-    background-size: cover;
-  }
-}
+    @media (max-width: 800px) {
+      .bg {
+        background-image: url('../../public/images/front1.jpg');
+        background-position: center center;
+        background-repeat: no-repeat;
+        position: fixed;
+        height: 100vh;
+        width: 100vw;
+        background-size: cover;
+      }
+    }
 
-@media (min-width: 801px) {
-  .bg {
-    background-image: url('../../public/images/front2.jpg');
-    background-position: center center;
-    background-repeat: no-repeat;
-    position: fixed;
-    height: 100vh;
-    width: 100vw;
-    background-size: cover;
-    overflow-y: scroll;
-  }
-}
+    @media (min-width: 801px) {
+      .bg {
+        background-image: url('../../public/images/front2.jpg');
+        background-position: center center;
+        background-repeat: no-repeat;
+        position: fixed;
+        height: 100vh;
+        width: 100vw;
+        background-size: cover;
+        overflow-y: scroll;
+      }
+    }
 </style>
