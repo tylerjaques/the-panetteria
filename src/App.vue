5bda020b5c65f6b56fa60607d6e00e011cd442b5
Tyler Jaques <Tyler.Jaques@edisonfinancial.ca>
2024-01-29 14:42:14 -0500

    trying again

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
   <div id="app">
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2VG4NGLB92"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-2VG4NGLB92');
-    </script>
     <div id="wrapper">
       <banner v-if="showBanner && bannerActive" @close="showBanner = false">
         We will be closed from January 1st to January 23rd and re-opening on January 24th, for our annual winter break.
